6339202e22389981624434cee27056c825a739af
SupriyaKotakonda <supriyakotakonda12@gmail.com>
2025-03-24 22:32:11 +0530

    24 March 2025 new

--- src/Tasks/Task001_Fibo_Series.java
+++ src/Tasks/Task001_Fibo_Series.java
@@ -1,6 +1,18 @@
 package Tasks;
 
 public class Task001_Fibo_Series {
-
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int c;
+        System.out.println(a);
+        System.out.println(b);
+        for (int i = 0; i < 10; i++) {
+            c = a + b;
+            System.out.println(c);
+            a = b;
+            b = c;
+        }
+    }
 
 }
