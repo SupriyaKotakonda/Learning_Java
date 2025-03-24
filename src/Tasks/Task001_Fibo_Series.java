252b6867c3bcbb7f0486838178424befb52e3aa2
SupriyaKotakonda <supriyakotakonda12@gmail.com>
2025-03-24 22:35:57 +0530

    24 March 2025 new laptop

--- src/Tasks/Task001_Fibo_Series.java
+++ src/Tasks/Task001_Fibo_Series.java
@@ -4,7 +4,7 @@ public class Task001_Fibo_Series {
     public static void main(String[] args) {
         int a = 0;
         int b = 1;
-        int c;
+        int c = 0;
         System.out.println(a);
         System.out.println(b);
         for (int i = 0; i < 10; i++) {
